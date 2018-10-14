1496889f5bd162239b69deb65cf7b5bf20cdf4d9
Carliitos <ccolado1998@gmail.com>
2018-10-14 12:43:09 +0200

    Star

--- javascript.js
+++ javascript.js
@@ -2,19 +2,37 @@ $(".post").on("click", function(){
    console.log("done!"); 
     
 });
-var night = false;
-$('.container').click(function() {
-    if(!night){
-    $(this).removeClass('container');
-    $(this).addClass('night');
-    night = true;
-    }else{
-    $(this).removeClass('night');
-    $(this).addClass('container');
-    night = false;
-    }
-});
+//var night = false;
+//$('.container').click(function() {
+//    if(!night){
+//    $(this).removeClass('container');
+//    $(this).addClass('night');
+//    night = true;
+//    }else{
+//    $(this).removeClass('night');
+//    $(this).addClass('container');
+//    night = false;
+//    }
+//});
 $(".search").on("click", function(){
     $( ".search" ).replaceWith("<input class='searchClick' type='text' placeholder='Buscar..'>" );
-    $(".search").select();
+    $(".searchClick").select();
+});
+var n =false;
+$(".fa-star").on("mouseenter", function(){
+    n=false;
+    console.log("enter");
+    $( ".fa-star" ).replaceWith("<i class='fas fa-star'></i>" );
+});
+$('body').on('mouseleave' , '.fa-star'  ,function(){
+    n=true;
+    console.log("dkifjn");
+    $(this).replaceWith("<i class='far fa-star'></i>" );
+});
+$("body").on("mouseenter",'.fa-star', function(){
+    if(n){
+    console.log("enter");
+    $( ".fa-star" ).replaceWith("<i class='fas fa-star'></i>" );
+        n=false;
+    }
 });
